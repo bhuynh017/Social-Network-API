7473e597f5bfae2c1b69fd126435b86354fae4f3
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 14:28:20 -0600

    adding method to get thoughts

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,3 +1,22 @@
 // destructuring two models: 
 // extracting Thought and User from the exported models folder.
 const { Thought, User } = require('../models');
+
+// gathering all thoughts
+const thoughtController = {
+    // retrieving info from the Thought and using the find() method from Mongoose.
+    getThoughts(req, res) {
+      Thought.find()
+      // sorted in descending order
+        .sort({ createdAt: -1 })
+        .then((dbThoughtData) => {
+            // returning data in JSON format.
+          res.json(dbThoughtData);
+        })
+        // showing an error.
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    },
+}
