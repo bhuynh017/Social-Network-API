fb4a8a09834630caee61771f3918ec8c72864ffb
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 16:36:31 -0600

    get getUsers and getSingleUser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,39 @@
 const { User, Thought } = require('../models');
+
+// getting all users
+const userController = {
+    // Retrieving info from user db
+    getUsers(req, res) {
+      User.find()
+      // select method is used to exclude the selected property from the data.
+        .select('-__v')
+        .then((dbUserData) => {
+          res.json(dbUserData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+    },
+
+      // get single user by id
+  getSingleUser(req, res) {
+    // retrieving info from the user db
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      // retrieving the relative data of friends and thoughts
+      .populate('friends')
+      .populate('thoughts')
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'There is no one with this ID!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  
+}
