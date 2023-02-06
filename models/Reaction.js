f07344f780fd63e6fbd6c172be08afe4bbb58ac6
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 16:48:44 -0600

    adding the modules needed for reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,8 @@
+// requriing two modules from the mongoose library.
+const { Schema, Types } = require('mongoose');
+// calling the utils dateFormat.js
+const dateFormat = require('../utils/dateFormat');
+
+const reactionSchema = new Schema(
+    
+);
--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+// exporting two modules User and Thought from models folder.
 const User = require('./User');
 const Thought = require('./Thought');
 
