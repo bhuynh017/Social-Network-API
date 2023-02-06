ebc1789f763652d4d142d51911da7e0d755af67d
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 14:41:18 -0600

    adding creae a thought and updating thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -20,6 +20,7 @@ const thoughtController = {
         });
     },
 
+    // getting a single thought by the ID.
     // defining a method called getSingleThought.
     getSingleThought(req, res) {
         // requesting for a document from the Thought.js
@@ -31,6 +32,48 @@ const thoughtController = {
             res.json(dbThoughtData);
           })
           // returning an error if occurs.
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+
+      // creating a thought
+      createThought(req, res) {
+        // creating a new Thought with the data from the request.
+        Thought.create(req.body)
+          .then((dbThoughtData) => {
+            // when successful then the created Thought id is added to the user thought id.
+            return User.findOneAndUpdate(
+              { _id: req.body.userId },
+              { $push: { thoughts: dbThoughtData._id } },
+              { new: true }
+            );
+          })
+          // if there is no data then it returns a 404 with the provided message.
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              return res.status(404).json({ message: 'The thought was created but there is no user with the id.' });
+            }
+    
+            // if there is data then the message provided is displayed. 
+            res.json({ message: 'The thought was created.' });
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+
+      // updating a thought.
+      updateThought(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true })
+          .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+              return res.status(404).json({ message: 'No thought with this id!' });
+            }
+            res.json(dbThoughtData);
+          })
           .catch((err) => {
             console.log(err);
             res.status(500).json(err);
