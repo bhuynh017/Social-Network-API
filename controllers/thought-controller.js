40114e4cd78451566bf4ef42e869023ba0a07290
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 15:27:32 -0600

    adding addReaction and removeReaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -106,4 +106,44 @@ const thoughtController = {
             res.status(500).json(err);
           });
       },
-}
+
+      // adding a reaction to the thought.
+      addReaction(req, res) {
+        Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $addToSet: { reactions: req.body } },
+          { runValidators: true, new: true }
+        )
+          .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+              return res.status(404).json({ message: 'No thought with this id!' });
+            }
+            res.json(dbThoughtData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+     
+      // removing the reactiong from the thought.
+      removeReaction(req, res) {
+        Thought.findOneAndUpdate(
+          { _id: req.params.thoughtId },
+          { $pull: { reactions: { reactionId: req.params.reactionId } } },
+          { runValidators: true, new: true }
+        )
+          .then((dbThoughtData) => {
+            if (!dbThoughtData) {
+              return res.status(404).json({ message: 'No thought with this id!' });
+            }
+            res.json(dbThoughtData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      },
+    };
+    
+    module.exports = thoughtController;
