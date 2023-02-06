5dbe3185ecb610062caf83d217cdcea27795f64e
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 16:38:22 -0600

    creating a user and updating a user added

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -36,4 +36,41 @@ const userController = {
       });
   },
   
+  // this is creating a user
+  // replicating what was built in the thought-controller.js
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => {
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  
+  // this is updating a current user.
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      {
+        $set: req.body,
+      },
+      {
+        runValidators: true,
+        new: true,
+      }
+    )
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          return res.status(404).json({ message: 'There is no one with this ID!' });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
 }
