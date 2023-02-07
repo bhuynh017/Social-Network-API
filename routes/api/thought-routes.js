95a7104a38ece499375c0ccb3adac1846bb85358
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 19:24:35 -0600

    adding require express getThoughts etc and linking it to thought-controller

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,11 @@
+// code requires thought-controller file and exporting multiple functions
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../../controllers/thought-controller');
