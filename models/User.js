6ba0e186b3789d1b02961d234a5b0e3280d069c4
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 19:00:08 -0600

    adding userSchema

--- models/User.js
+++ models/User.js
@@ -1,2 +1,38 @@
 // requiring two modules from the mongoose library.
 const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Email address must match!'],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought',
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+      },
+      id: false,
+    }
+  );
+
