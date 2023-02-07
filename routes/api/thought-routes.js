3a77a55ba1c6d1ddbdfe810ff459831c86e0de1e
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 19:30:08 -0600

    exporting different routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -9,3 +9,18 @@ const {
   addReaction,
   removeReaction,
 } = require('../../controllers/thought-controller');
+
+// exporting different routes.
+// GET request with getThoughts and post with createThought.
+router.route('/').get(getThoughts).post(createThought);
+
+// GET getSingleThoughtm PUT request updateThought and then DELETE deleteThought.
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+// defining a route where it handles POST request.
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// handling DELETE request.
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
