0b3e2187e07d2caf455fd17db068a8f4b2361d05
Bryant Huynh <bryanthuynh@Bryants-MacBook-Pro.local>
2023-02-06 19:08:21 -0600

    adding friend count function

--- models/User.js
+++ models/User.js
@@ -1,38 +1,45 @@
 // requiring two modules from the mongoose library.
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-      username: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Email address must match!'],
-      },
-      thoughts: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'Thought',
-        },
-      ],
-      friends: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'User',
-        },
-      ],
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    {
-      toJSON: {
-        virtuals: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/, "Email address must match!"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
       },
-      id: false,
-    }
-  );
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
